ba88b79c09785b7c122ee18a7df383d7e0c47dba
AJuppin <anthony.juppin96@gmail.com>
2023-03-06 12:08:26 +0100

    premier commit

---
+++ src/main/java/com/intiFormation/ProjetCentreFormationApplication.java
@@ -0,0 +1,13 @@
+package com.intiFormation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCentreFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCentreFormationApplication.class, args);
+	}
+
+}
