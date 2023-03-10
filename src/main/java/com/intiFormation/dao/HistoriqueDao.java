33c6f8cd43d3412129e4c53cbc8193322616b2ba
AJuppin <anthony.juppin96@gmail.com>
2023-03-10 09:08:56 +0100

    page commercial

--- src/main/java/com/intiFormation/dao/HistoriqueDao.java
+++ src/main/java/com/intiFormation/dao/HistoriqueDao.java
@@ -1,10 +1,16 @@
 package com.intiFormation.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.intiFormation.entity.Formation;
 import com.intiFormation.entity.Historique;
 
 public interface HistoriqueDao extends JpaRepository <Historique, Integer> {
+	
+	public List<Historique> findByPersonne_Id(int id);
 	
 	
 
